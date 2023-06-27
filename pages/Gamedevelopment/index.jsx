63284a4e034f522be7fecef83673fe16f93ce6ad
aman-tanav <anantanav4848.com>
2023-06-27 20:49:27 +0530

    rbs

---
+++ pages/Gamedevelopment/index.jsx
@@ -0,0 +1,76 @@
+"user client"
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from 'react'
+
+export default function GameDevelopment() {
+  return (
+    <div>
+      <div className='game-dev'>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h2 className=' text-center'>GAME DEVELOPMENT</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* development-head */}
+
+      <div className="develop-head">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="browse-inner-data">
+                <h2 className=''>BROWSE OUR GAME DEVELOPMENT SEGMENTS</h2>
+                <p>Get ready to level up your gaming experience with our top-notch game development services.
+                   Our team of experienced developers creates engaging games with stunning graphics and seamless performance.
+                    From concept to launch, We will bring your gaming vision to life.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+{/* games-categories */}
+
+<div className="games-catgr">
+  <div className="container">
+    <div className="row justify-content-between">
+      <div className="col-md-2">
+        <div className="gm-ct">
+          <h3>ACTION GAMES</h3>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="gm-ct">
+          <h3>ACTION GAMES</h3>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="gm-ct">
+          <h3>ACTION GAMES</h3>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="gm-ct">
+          <h3>ACTION GAMES</h3>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="gm-ct">
+          <h3>ACTION GAMES</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    </div>
+
+  )
+}
+
